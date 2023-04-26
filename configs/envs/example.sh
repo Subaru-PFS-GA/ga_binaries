ae1494c7855c32c2a9b403805e4e71b6e88c8872
Laszlo Dobos <dobos@jhu.edu>
2023-04-26 18:51:37 -0400

    Initial git check-in

---
+++ configs/envs/example.sh
@@ -0,0 +1,8 @@
+export PFS_BINARIES_CONDAPATH="/usr/local/miniconda3"
+export PFS_BINARIES_CONDAENV="binaries"
+export PFS_BINARIES_ROOT=~/project/ga_binaries
+export PFS_BINARIES_DATA=~/data/binaries
+export PFS_BINARIES_TEST=~/data/binaries/test
+export PFS_BINARIES_DEBUGPORT=5689
+
+export PYTHONPATH=""
